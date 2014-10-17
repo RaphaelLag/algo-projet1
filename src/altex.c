566fdcb5dc7f1009b2c4d745e7ddda0ec5474324
Raphaël Laguerre <raphael.laguerre@ensimag.imag.fr>
2014-10-17 22:07:40 +0200

    implémentation du calcul du cout minimum. SEG FAULTw

--- src/altex.c
+++ src/altex.c
@@ -19,6 +19,60 @@
  */
 //static void free_tab(char** tab, int tab_len);
 
+
+/**
+ * Computes the optimal justification of a parapgraph according to the following
+ * algorithm: 
+ * L (i,k) = L(m_k) + sum(L(m_j) + mu) (j=1,j<=k-1)
+ * E(i,k) = M - L(i,k);
+ * phi(i) {
+ *  if (E(i,n) >= 0) return 0;
+ *  min = M
+ *  k = 0
+ *  while (k<n && E(i,k)>=0) {  
+ *      if (aux = phi(k+1) + N(E(i,k)) < min) min = aux;
+ *      k++;
+ *  }
+ *      
+ *  return min;
+ * 
+ * @param i: we compute from the word at the index i of tabwords
+ * @param outformat: output stream
+ * @param M: line size (in units)
+ * @param N: function to optimize is #spaces^N 
+ * @param nbwords: number of words in the paragraph considered
+ * @param tabwords: array of the words in the paragraph considered
+ * @size_separator: the minimum size of the white space between two consecutive words on a line
+ * @param return the cost for an optimal alignment for the parapgraph considered 
+ */
+static int justify_par(int i, struct stream* outformat, unsigned long M,
+                unsigned N, int nbwords, char** tabwords, long size_separator);
+/**
+ * Computes the space size to add in order to make a line composed of the words from the
+ * ith word till the nth word have a total size of M.
+ * @param i: we compute from the word at the index i of tabwords
+ * @param n: we compute till the word at the index n of tabwords
+ * @param M: line size (in units)
+ * @param tabwords: array of the words in the paragraph considered
+ * @param outformat: output stream
+ * @size_separator: the minimum size of the white space between two consecutive words on a line
+ * @param return the size (in units) to add
+ */
+static int E(int i, int n, unsigned long M, char** tabword,
+                struct stream* outformat, long size_separator);
+/**
+ * return the size(in units) of the words and the minimum spaces between words for the
+ * ith word till the nth word.
+ * @param i: we compute from the word at the index i of tabwords
+ * @param n: we compute till the word at the index n of tabwords
+ * @param tabwords: array of the words in the paragraph considered
+ * @param outformat: output stream
+ * @size_separator: the minimum size of the white space between two consecutive words on a line
+ * @param return the size (in units) of the words between the indexes i and n 
+ */
+static int L(int i, int n, char** tabwords, struct stream* outformat, long
+                size_separator);
+
 /** Optimal alignment of a text (read from input file in) with ligns of fixed length (M units) written on output file (out).
  * @param in: input stream, in ascii (eg in == stdin)
  * @param len: maximum number of words in the text (may be larger than the stream)
@@ -28,24 +82,14 @@
  * @return: the cost of an optimal alignment
  */
 long altex(FILE* in, size_t len, struct stream *outformat, unsigned long M, unsigned N) ;
-/**
- * L (i,k) = L(m_k) + sum(L(m_j) + mu) (j=1,j<=k-1)
- * E(i,k) = M - L(i,k);
- * phi(i) {
- *  if (E(i,n) >= 0) return 0;
- *  min = max_entier
- *  for (int k = 0; k<n; k++ ) {
- *      if (aux = phi(k+1) + N(E(i,k)) < min) min = aux; *     
- *  }
- *      
- *  return min;
- */
 
-long altex(FILE* in, size_t len, struct stream *outformat, unsigned long M, unsigned N) {
+long altex(FILE* in, size_t len, struct stream *outformat, unsigned long M,
+                unsigned N)
+{
         char* buffer = (char*) calloc(len+1, sizeof(char) ) ;  // Preallocated buffer, large enough to store any word...
-        size_t buffer_size = len ; 
-        struct parser p_in; 
-        init_parser(in, &p_in) ; 
+        size_t buffer_size = len ;
+        struct parser p_in;
+        init_parser(in, &p_in) ;
         long maxval_all_paragraphs = 0 ;
         long sumval_all_paragraphs = 0 ;
         int endofile = 0;
@@ -70,11 +114,13 @@ long altex(FILE* in, size_t len, struct stream *outformat, unsigned long M, unsi
                           }*/
                 }
                 par_len += size_separator * (nbwords - 1);
-                // Case were length of the paragraph is less than M
+                // Case where length of the paragraph is less than M
                 if (par_len < M) { // We write the paragraph in the output
                         draw_wordline(outformat, nbwords, tabwords, 1);
                 }
-                //      draw_wordline(outformat, nbwords, tabwords, 1);
+                // else we recursively compute the optimal jusitfication 
+                sumval_all_paragraphs += justify_par(0, outformat, M, N, nbwords,
+                                tabwords, size_separator);
                 //      on réinitialise les variables nécessaires pour le prochain paragraphe
                 //      + on libère la mémoire
                 par_len = 0;
@@ -84,7 +130,8 @@ long altex(FILE* in, size_t len, struct stream *outformat, unsigned long M, unsi
         }
         free(tabwords);
         free(buffer) ;
-        fprintf(stderr, "\n#@alignment_cost:MAX=%ld;SUM=%ld\n", maxval_all_paragraphs, sumval_all_paragraphs);
+        fprintf(stderr, "\n#@alignment_cost:MAX=%ld;SUM=%ld\n",
+                        maxval_all_paragraphs, sumval_all_paragraphs);
 
         return maxval_all_paragraphs;
 }
@@ -206,3 +253,50 @@ int main(int argc, char *argv[] ) {
    }
    */
 
+static int justify_par(int i, struct stream* outformat, unsigned long M,
+                unsigned N, int nbwords, char** tabwords, long size_separator)
+{
+        int min = M;
+        int k = 0;
+        int aux, nbspaces;
+        if (E(i, nbwords, M, tabwords, outformat, size_separator) >= 0)
+                return 0;
+        while (k < nbwords && (nbspaces = E(i, k, M, tabwords, outformat,
+                                        size_separator)) >= 0) {
+                if ((aux = justify_par(k+1, outformat, M, N, nbwords, tabwords,
+                                                size_separator) + penality(nbspaces, N))
+                                >= 0) min = aux;
+                k++;
+        }
+        return min;
+}
+/**
+ * L (i,k) = L(m_k) + sum(L(m_j) + mu) (j=1,j<=k-1)
+ * E(i,k) = M - L(i,k);
+ * phi(i) {
+ *  if (E(i,n) >= 0) return 0;
+ *  min = max_entier
+ *  while (k<n && E(i,k)>=0) {  
+ *      if (aux = phi(k+1) + N(E(i,k)) < min) min = aux;     
+ *  }
+ *      
+ *  return min;
+ */
+
+static int E(int i, int n, unsigned long M, char** tabwords,
+                struct stream* outformat, long size_separator)
+{
+        return (M - L(i, n, tabwords, outformat, size_separator));
+}
+
+static int L(int i, int n, char** tabwords, struct stream* outformat, long
+                size_separator)
+{
+        int words_n_spaces_length = wordlength(outformat, tabwords[0]);
+        int k;
+        for (k = i+1; k <= n; k++) {
+                words_n_spaces_length += wordlength(outformat, tabwords[i]) +
+                        size_separator;
+        }
+        return words_n_spaces_length;
+}
